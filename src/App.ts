80833a1f402156976f74b5cd905b5c6e4af7135f
ninh.pham <manhninh91@gmail.com>
2021-04-14 16:38:07 +0700

    remove route not use

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,4 @@
-import {errorMiddleware, notFoundMiddleware} from 'bo-trading-common/lib/utils';
-import {json, urlencoded} from 'body-parser';
-import compression from 'compression';
 import express from 'express';
-import routes from './routes';
 import Scheduler from './schedulers';
 import HeartBeat from './socketHandlers/candlestickStreams/HeartBeat';
 import Indicator from './socketHandlers/indicator';
@@ -13,30 +9,11 @@ class App {
 
   constructor() {
     this.app = express();
-    this.config();
     /** lắng nghe dữ liệu nến trả về từ các sàn (Binance,...) để xử lý nến */
     new HeartBeat();
     new Indicator();
     /** cronjob */
     new Scheduler().config();
-  }
-
-  private config() {
-    // this.app.use(cors({origin: '*', methods: ['PUT', 'POST', 'GET', 'DELETE', 'OPTIONS']}));
-    this.app.use(compression());
-
-    /** support application/json type post data */
-    this.app.use(json({limit: '10MB'}));
-    this.app.use(urlencoded({extended: true}));
-
-    /** add routes */
-    this.app.use('/api/v1', routes);
-
-    /** not found error */
-    this.app.use(notFoundMiddleware);
-
-    /** internal server Error  */
-    this.app.use(errorMiddleware);
   }
 }
 
