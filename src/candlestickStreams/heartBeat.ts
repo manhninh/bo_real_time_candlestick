752a9293b5240ef65d2f53ab53807644b01c73fd
Ninh Phạm <manhninh91@gmail.com>
2021-06-11 19:33:53 +0700

    update code

--- src/candlestickStreams/heartBeat.ts
+++ src/candlestickStreams/heartBeat.ts
@@ -1,11 +1,11 @@
-import {PROTECT_STATUS} from '@src/contants/system';
+import { PROTECT_STATUS } from '@src/contants/system';
 import BlockRepository from '@src/repository/blockRepository';
 import LastResultRepository from '@src/repository/lastResultRepository';
-import {IBlockModel} from 'bo-trading-common/lib/models/blocks';
-import {ILastResultModel} from 'bo-trading-common/lib/models/lastResults';
-import {logger} from 'bo-trading-common/lib/utils';
+import { IBlockModel } from 'bo-trading-common/lib/models/blocks';
+import { ILastResultModel } from 'bo-trading-common/lib/models/lastResults';
+import { logger } from 'bo-trading-common/lib/utils';
 import moment from 'moment';
-import {EMITS} from '../socketHandlers/emitType';
+import { EMITS } from '../socketHandlers/emitType';
 
 export default class HeartBeat {
   private _blockRes: BlockRepository;
@@ -33,26 +33,38 @@ export default class HeartBeat {
       totalVolume += global.candlestick.v;
 
       /** gán lại giá trị close của nến trước cho giá trị open của nến hiện tại */
+      console.log(closeToOpen, "closeToOpen");
       this._candlestick.o = closeToOpen || global.candlestick.o;
 
-      /** gán lại giá trị hight price */
-      if (hPrice) hPrice = global.candlestick.h >= hPrice ? global.candlestick.h : hPrice;
-      else hPrice = closeToOpen || global.candlestick.o;
+      // gán giá trị close hiện tại vào biến để so sánh
+      closeToOpen = global.candlestick.c;
+
+      /** gán lại giá trị hight price khi so sánh với kết quả cuối cùng của nến */
+      if (hPrice) {
+        hPrice = closeToOpen >= hPrice ? closeToOpen : hPrice;
+      } else {
+        hPrice = this._candlestick.o;
+      }
       this._candlestick.h = hPrice;
 
       /** gán lại giá trị low price */
-      if (lPrice) lPrice = global.candlestick.l <= lPrice ? global.candlestick.l : lPrice;
-      else lPrice = closeToOpen || global.candlestick.o;
+      if (lPrice) {
+        lPrice = closeToOpen <= lPrice ? closeToOpen : lPrice;
+      }else {
+        lPrice = this._candlestick.o;
+      }
       this._candlestick.l = lPrice;
 
       // tính năng bảo vệ sàn
       if (global.protectBO === PROTECT_STATUS.BUY_WIN) {
+        console.log("buy win")
         if (this._candlestick.o > global.candlestick.c) {
           const rangeData = this._candlestick.o - global.candlestick.c;
           const fake = this._randomRange(this._candlestick.c, this._candlestick.o);
           this._candlestick.c += rangeData + fake;
         }
       } else if (global.protectBO === PROTECT_STATUS.SELL_WIN) {
+        console.log("sell win")
         if (this._candlestick.o < global.candlestick.c) {
           const rangeData = global.candlestick.c - this._candlestick.o;
           const fake = this._randomRange(this._candlestick.o, this._candlestick.c);
@@ -64,7 +76,7 @@ export default class HeartBeat {
 
       /** nếu ở giây thứ 0 hoặc 30 thì sẽ lấy giá trị close lưu lại  */
       if (timeTick === 0 || timeTick === 30) {
-        closeToOpen = this._candlestick.c > global.candlestick.c ? this._candlestick.c : global.candlestick.c;
+        // closeToOpen = this._candlestick.c > global.candlestick.c ? this._candlestick.c : global.candlestick.c;
         hPrice = null;
         lPrice = null;
       }
@@ -86,6 +98,16 @@ export default class HeartBeat {
 
       /** emit to room ethusdt */
       global.io.sockets.in('ethusdt').emit(EMITS.ETHUSDT_REALTIME, data);
+
+      if (timeTick >= 28 && timeTick <= 30 || timeTick === 0) {
+        console.log("data", timeTick, {
+          event_time: eventTime,
+          open: this._candlestick.o,
+          close: this._candlestick.c,
+          high: this._candlestick.h,
+          low: this._candlestick.l
+        })
+      }
 
       /** cách 30s lưu giá trị vào bảng block 1 lần */
       if (timeTick === 0 || timeTick === 30) {
